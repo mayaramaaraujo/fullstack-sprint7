6872a20cce73fc23904033b2da2e48c4ca4428c1
DESKTOP-JVIJUL5\55659 <mayaramaaraujo98@gmail.com>
2021-08-13 14:38:29 -0300

    #4 product testing correction

--- src/test/java/br/com/rchlo/store/repository/ProductRepositoryTest.java
+++ src/test/java/br/com/rchlo/store/repository/ProductRepositoryTest.java
@@ -1,19 +1,28 @@
 package br.com.rchlo.store.repository;
 
+
 import br.com.rchlo.store.domain.Color;
 import br.com.rchlo.store.domain.Product;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -21,43 +30,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ActiveProfiles("test")
 class ProductRepositoryTest {
 
-    @Autowired
-    private TestEntityManager entityManager;
+  @Autowired
+  private TestEntityManager entityManager;
 
-    @Autowired
-    private ProductRepository productRepository;
+  @Autowired
+  private ProductRepository productRepository;
 
-    @Test
-    void shouldListAllProductsOrderedByName() {
-        entityManager.persist(new Product(7L,
-                "Jaqueta Puffer Juvenil Com Capuz Super Mario Branco",
-                "A Jaqueta Puffer Juvenil Com Capuz Super Mario Branco é confeccionada em material sintético.",
-                "jaqueta-puffer-juvenil-com-capuz-super-mario-branco-13834193_sku",
-                "Nintendo",
-                new BigDecimal("199.90"),
-                null,
-                Color.WHITE,
-                147));
-        entityManager.persist(new Product(1L,
-                "Regata Infantil Mario Bros Branco",
-                "A Regata Infantil Mario Bros Branco é confeccionada em fibra natural. Aposte!",
-                "regata-infantil-mario-bros-branco-14040174_sku",
-                "Nintendo",
-                new BigDecimal("29.90"),
-                null,
-                Color.WHITE,
-                98));
+  @Test
+  void shouldListAllProductsOrderedByName() {
+    entityManager.persist(new Product(7L,
+            "Jaqueta Puffer Juvenil Com Capuz Super Mario Branco",
+            "A Jaqueta Puffer Juvenil Com Capuz Super Mario Branco é confeccionada em material sintético.",
+            "jaqueta-puffer-juvenil-com-capuz-super-mario-branco-13834193_sku",
+            "Nintendo",
+            new BigDecimal("199.90"),
+            null,
+            Color.WHITE,
+            147));
+    entityManager.persist(new Product(1L,
+            "Regata Infantil Mario Bros Branco",
+            "A Regata Infantil Mario Bros Branco é confeccionada em fibra natural. Aposte!",
+            "regata-infantil-mario-bros-branco-14040174_sku",
+            "Nintendo",
+            new BigDecimal("29.90"),
+            null,
+            Color.WHITE,
+            98));
 
-        List<Product> products = productRepository.findAll();
 
-        assertEquals(2, products.size());
+    Pageable pagination = PageRequest.of(0, 10, Sort.by("name"));
+    List<Product> products = productRepository.findAll(pagination).toList();
 
-        Product firstProduct = products.get(0);
-        assertEquals(7L, firstProduct.getCode());
-        assertEquals("Jaqueta Puffer Juvenil Com Capuz Super Mario Branco", firstProduct.getName());
+    assertEquals(2, products.size());
 
-        Product secondProduct = products.get(1);
-        assertEquals(1L, secondProduct.getCode());
-        assertEquals("Regata Infantil Mario Bros Branco", secondProduct.getName());
-    }
+    Product firstProduct = products.get(0);
+    assertEquals(7L, firstProduct.getCode());
+    assertEquals("Jaqueta Puffer Juvenil Com Capuz Super Mario Branco", firstProduct.getName());
+
+    Product secondProduct = products.get(1);
+    assertEquals(1L, secondProduct.getCode());
+    assertEquals("Regata Infantil Mario Bros Branco", secondProduct.getName());
+  }
+
 }
